165b87c8de697791b3f4fe160aa74782b3cb8924
Irvan Fahreza <55876361+irvanfahreza@users.noreply.github.com>
2022-12-14 11:53:29 +0700

    Add files via upload

---
+++ polymorphism/Manager.java
@@ -0,0 +1,12 @@
+package Polymorphism;
+
+public class Manager extends Employee{
+
+    Manager (String name){
+        super(name);
+
+    }
+    void sayHello(){
+        System.out.println("Hello Manager "+this.name);
+    }
+}
